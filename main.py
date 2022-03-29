4527b60b9048eb51d376bef1d5ee90583495dfd6
ljxk <102322415+ljxk@users.noreply.github.com>
2022-03-29 19:25:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+class dog:
+  def __init__(self,_furcolor,_size,_breed,_hasdroopyears,_gender):
+    self.furcolor = _furcolor
+    self.size = _size
+    self.breed = _breed
+    self.droopyears = _hasdroopyears
+    self.gender = _gender
+    
+
+dog1 = dog("brown","small","chihuahua",False,"male")
